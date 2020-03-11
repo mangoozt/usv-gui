c4835177720ce24936fdc9ccce419479932c2278
Mark Tartagashev <mangoozt@gmail.com>
2020-03-11 16:49:08 +0300

    First triangle

---
+++ oglwidget.h
@@ -0,0 +1,21 @@
+#ifndef OGLWIDGET_H
+#define OGLWIDGET_H
+
+#include <QWidget>
+#include <QOpenGLWidget>
+#include <gl/GLU.h>
+#include <gl/GL.h>
+
+class OGLWidget : public QOpenGLWidget
+{
+public:
+    OGLWidget(QWidget *parent = 0);
+    ~OGLWidget();
+
+protected:
+    void initializeGL();
+    void resizeGL(int w, int h);
+    void paintGL();
+};
+
+#endif // OGLWIDGET_H
