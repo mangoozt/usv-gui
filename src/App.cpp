2799038bf61381a22452d0948824f047a300b0c5
Mark Tartagashev <mangoozt@gmail.com>
2021-06-07 20:33:45 +0300

    Fix reload in working directory

--- src/App.cpp
+++ src/App.cpp
@@ -11,7 +11,7 @@
 #define USV_GUI_USV_EXECUTABLE_ENV_NAME "USV_GUI_USV_EXECUTABLE"
 
 void App::run() {
-    load_directory("");
+    load_directory(std::filesystem::current_path());
     // Main loop
     while (!glfwWindowShouldClose(window)) {
         // Check if any events have been activated (key pressed, mouse moved etc.) and call corresponding response functions
